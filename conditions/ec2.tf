0fbb22d7aaf03f40e53588b9b04d6fbf5fbeafcf
NArunkumar <nelluri.arunkumar@gmail.com>
2025-08-20 09:05:38 +0530

    Terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "Roboshop" {
+    ami = var.ami_id
+    instance_type = var.environment == "dev" ? "t2.micro" : "t3.micro"
+    vpc_security_group_ids = [ aws_security_group.allow-all.id ]
+    tags = {
+        Name = "HelloTerraform"
+    }
+}
+
+resource "aws_security_group" "allow-all" {
+    name = var.sg_name
+    description = var.sg_description
+
+    ingress {
+        from_port = var.from_port
+        to_port = var.to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port = var.from_port
+        to_port = var.to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = var.sg_tags
+}
---
+++ loops/variables.tf
@@ -0,0 +1,62 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of joindevops"
+}
+
+variable "instance_type" {
+    default = "t2.micro"  
+}
+
+variable "ec2_tags" {
+    type = map(string)
+    default = {
+      Name = "HelloTerraform"
+      purpose = "variables-demo"
+    }
+}
+
+variable "sg_name" {
+    default = "allow-all"  
+}
+
+variable "sg_description" {
+    default = "Allowing all ports from internet"  
+}
+
+variable "from_port" {
+    default = 0  
+}
+
+variable "to_port" {
+    type = number
+    default = 0  
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = [ "0.0.0.0/0" ]
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
+
+variable "environment" {
+  default = "prod"
+}
+
+variable "instances" {
+    default = ["mongodb", "redis", "mysql", "rabbitmq"]
+  
+}
+
+variable "zone_id" {
+    default = "Z0541638YFS78XZ9O3N4"
+}
+
+variable "domain_name" {
+    default = "arunkumarnelluri.site"  
+}
