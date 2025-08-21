963016c9d36539b71f30fad86e92135c4d563362
NArunkumar <nelluri.arunkumar@gmail.com>
2025-08-21 17:55:59 +0530

    Terraform

---
+++ state/provider.tf
@@ -0,0 +1,23 @@
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.98.0"
+        }
+    }
+
+
+    backend "s3" {
+        bucket = "mybucket-for-terraform-statefile"
+        key = "remote-state"
+        region = "us-east-1"
+        #dynamodb_table = "table-locking-terraform-statefile"
+        encrypt = true
+        use_lockfile = true ##Enable s3 native locking
+    
+    }
+}
+
+provider "aws" {
+    #configuration options
+}
